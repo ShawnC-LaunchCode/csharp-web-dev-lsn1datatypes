de673fb6c7efd55c49367da6e83e745a2597cc5d
Shawn C <scooter4356@gmail.com>
2020-06-23 12:12:37 -0500

    test

---
+++ InputOutput/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = "What is your name";
+            string name;
+            string greeting = "Hiya";
+
+            Console.WriteLine(prompt);
+            name = Console.ReadLine();
+            Console.WriteLine(greeting + " " + name);
+
+        }
+    }
+}
